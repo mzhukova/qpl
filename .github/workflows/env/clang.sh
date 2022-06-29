71d085b4c680554acae19f6cd9a4d63c2beb4e79
Zhukova, Maria <maria.zhukova@intel.com>
2022-06-29 15:52:33 -0700

    github: add build (gcc, clang) and testing on latest ubuntu

---
+++ .github/workflows/env/clang.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# ==========================================================================
+# Copyright (C) 2022 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+# ==========================================================================
+
+sudo apt-get update -y
+sudo apt-get -y install clang
---
+++ .github/workflows/env/nasm.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# ==========================================================================
+# Copyright (C) 2022 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+# ==========================================================================
+
+sudo apt-get update -y
+sudo apt-get install -y nasm
+
